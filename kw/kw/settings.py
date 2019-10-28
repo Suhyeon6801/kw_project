7e889ffe4f337d65eec42fb042d63c38dac67cbd
Suhyeon Kim <k0s3y0@gmail.com>
2019-10-28 16:54:08 +0900

    u

--- kw/kw/settings.py
+++ kw/kw/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework'
+    'rest_framework',
     'myapp.apps.MyappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
